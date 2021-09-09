9267c4ec187288da49e11aa12296030fd9c4571c
Imam Arief Rahman <imam@Imams-MacBook-Pro.local>
2021-09-09 09:26:04 +0700

    deploy core-payment

---
+++ service_deployment/core-payment/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  backend "s3" {
+    bucket  = "avn-dev-storage-s3-terraform"
+    region  = "ap-southeast-2"
+    key     = "avn-core-payment-dev.tfstate"
+    profile = "avn-dev"
+  }
+}
+
+data "template_file" "iam_role_task_policy" {
+  template = file("task_policy.json")
+}
+
+module "deployment" {
+  source                   = "../../modules/deployment"
+  region                   = "ap-southeast-2"
+  unit                     = "avn"
+  env                      = "dev"
+  code                     = "core"
+  feature                  = "payment"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 128
+  memory                   = 512
+  memory_reservation       = 128
+  container_port           = 80
+  protocol                 = "TCP"
+  target_type              = "ip"
+  deregistration_delay     = "30"
+  health_check_interval    = "30"
+  healthy_threshold        = 5
+  unhealthy_threshold      = 5
+  network_mode             = "awsvpc"
+  launch_type              = "FARGATE"
+  iam_policy               = data.template_file.iam_role_task_policy.rendered
+  predefined_metric_type   = ["ECSServiceAverageCPUUtilization"]
+  min_capacity             = 2
+  max_capacity             = 8
+  target_value             = [175]
+  policy_type              = "TargetTrackingScaling"
+  scale_in_cooldown        = 15
+  scale_out_cooldown       = 5
+}
